8cc4196489db23cb1c317821fcd52ef429ddb902
matthaussouza <71097104+matthaussouza@users.noreply.github.com>
2021-11-15 16:47:22 -0300

    Atualizando os arquivos do projeto

---
+++ experiencein/perfis/urls.py
@@ -0,0 +1,11 @@
+from django.urls import include, path
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('perfis/<int:perfil_id>', views.exibir, name='exibir'),
+    path('perfis/<int:perfil_id>/convidar', views.convidar, name='convidar')
+]
